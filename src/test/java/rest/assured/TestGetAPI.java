a9f3975d0c5908297ac2fc8268e8607407c4a7f9
shaurya <shaurya@shauryas-Laptop.local>
2024-04-14 11:35:28 +0530

    added schema validation

--- src/test/java/rest/assured/TestGetAPI.java
+++ src/test/java/rest/assured/TestGetAPI.java
@@ -1,9 +1,14 @@
 package rest.assured;
 
+import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.ValidatableResponse;
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
+
 import static io.restassured.RestAssured.given;
+import static org.apache.http.HttpStatus.SC_OK;
 
 public class TestGetAPI {
 
@@ -12,7 +17,11 @@ public class TestGetAPI {
         String resource = "https://reqres.in/api/users/2";
         ValidatableResponse response = given().
         when().get(resource).
-        then().statusCode(200);
-        response.log().body();
+        then().statusCode(200).
+        and().assertThat().body(JsonSchemaValidator.matchesJsonSchema(new File("src/test/java/rest/assured/schema.json")));
+
+        int statusCode = response.extract().statusCode();
+        Assert.assertEquals(statusCode, SC_OK);
     }
+
 }
